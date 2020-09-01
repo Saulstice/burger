d239ab5a9f1b34c769f3177f49f0f50c59783825
Sol <saulsallah1@gmail.com>
2020-09-01 17:48:22 -0400

    deploy through heroku

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# burger
-Create a burger logger with MySQL, Node, Express, Handlebars and an ORM. Used Node and MySQL to query and route data in app, and Handlebars to generate the HTML.
+# Burger Logger
+
+Eat-da-burger is a logger created with MySQL, Node, Express, Handlebars, and a custom ORM. Node and MySQL used to query and route data in the app.
+
+
+## Usage
+
+Enter in a new burger and then click 'Add Burger' to log it. Once logged you can click 'Eat it' to update the burger entry to 'Eaten'.
--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Sallah28",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "Sallah28",
+    database: "burgers_db"
+  });
+}
 
 // Connect to the database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
